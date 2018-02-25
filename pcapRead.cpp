343c0390721d9fe2ec7386b5aa0bb866d849d643
PritomKumar <bsse0919@iit.du.ac.bd>
2018-02-26 01:51:23 +0600

    code change

--- pcapRead.cpp
+++ pcapRead.cpp
@@ -5,15 +5,23 @@
 
 using namespace std;
 
-typedef struct pcapGlobalHeader {
+typedef struct pcapGlobalHeader { 	//total 24 bytes
 
         char magicNumber[4];   		// magic number , 4 byte
         char majorVersionNumber[2];  // major version number ,2 byte
         char minorVersionNumber[2];  // minor version number , 2 byte
         char timeOffsetGMT[4];       // GMT to local correction , 4 byte
-        char sigfigs[4];        		// accuracy of timestamps , 4 byte
+        char sigfigs[4];        	// accuracy of timestamps , 4 byte
         char maxSnapshotLenght[4];    // max length of captured packets, in octets , 4 byte
         char linkLayerProtocol[4];    // data link type , 4 byte
+};
+
+typedef struct packetHeader{
+		char timeStamps[4];				// timestamps in seconds from 1970
+		char packetCaptureTime[4];		// capture time in microseconds
+		char packetSizeFromData[4];		// saved data size in packets
+		char packetLengthFromWire[4];	// packet length captured from wire
+
 };
 
 int main(){
@@ -27,13 +35,21 @@ int main(){
 	fp = fopen("samplePcap.pcap","rb");
 
 	pcapGlobalHeader globhdr;
+	packetHeader  pachdr;
 
 	fread(&globhdr, sizeof(struct pcapGlobalHeader), 1, fp);
+	fread(&pachdr , sizeof(struct packetHeader) , 1 , fp);
 
 	for(int i =0 ; i<4 ; i++){
-		cout << globhdr.magicNumber[i] << " " ; //hahahahhahaha I am a genius;
+		//cout << globhdr.magicNumber[i] << " " ; //hahahahhahaha I am a genius;
+		printf("%.02x " , globhdr.magicNumber[i]&(0xff));
 	}
 
+	for(int i =0 ; i<4 ; i++){
+		//cout << globhdr.magicNumber[i] << " " ; //hahahahhahaha I am a genius;
+		printf("%.02x " , pachdr.timeStamps[i]&(0xff));
+	}
+// just need to convert the numbers in appropriate hex value and convert them to integer
 	int i=0;
 /*
 	while(!feof(fp)){
